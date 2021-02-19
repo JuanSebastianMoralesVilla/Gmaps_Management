a3598df0379afd29a9bd75cf776d9c19e237e028
JuanSebastianMoralesVilla <juansebastianmoralesvilla@gmail.com>
2021-02-19 17:11:42 -0500

    Instalacion de gmap y carga de datos

--- Gmaps_Management/Gmaps_Management/Form1.Designer.cs
+++ Gmaps_Management/Gmaps_Management/Form1.Designer.cs
@@ -29,13 +29,84 @@ namespace Gmaps_Management
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txloadFile = new System.Windows.Forms.TextBox();
+            this.txBoxload2 = new System.Windows.Forms.TextBox();
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 72);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cargar Archivo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txloadFile
+            // 
+            this.txloadFile.Location = new System.Drawing.Point(222, 22);
+            this.txloadFile.Name = "txloadFile";
+            this.txloadFile.Size = new System.Drawing.Size(490, 20);
+            this.txloadFile.TabIndex = 1;
+            // 
+            // txBoxload2
+            // 
+            this.txBoxload2.Location = new System.Drawing.Point(222, 74);
+            this.txBoxload2.Name = "txBoxload2";
+            this.txBoxload2.Size = new System.Drawing.Size(490, 20);
+            this.txBoxload2.TabIndex = 2;
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl1.GrayScaleMode = false;
+            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl1.LevelsKeepInMemmory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(412, 110);
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 2;
+            this.gMapControl1.MinZoom = 2;
+            this.gMapControl1.MouseWheelZoomEnabled = true;
+            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(579, 474);
+            this.gMapControl1.TabIndex = 3;
+            this.gMapControl1.Zoom = 0D;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1048, 632);
+            this.Controls.Add(this.gMapControl1);
+            this.Controls.Add(this.txBoxload2);
+            this.Controls.Add(this.txloadFile);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txloadFile;
+        private System.Windows.Forms.TextBox txBoxload2;
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
     }
 }
 
--- Gmaps_Management/Gmaps_Management/Form1.cs
+++ Gmaps_Management/Gmaps_Management/Form1.cs
@@ -12,9 +12,28 @@ namespace Gmaps_Management
 {
     public partial class Form1 : Form
     {
+        // ruta del archivo
+        private string path;
+
         public Form1()
         {
+            //Ubicacion del archivo a cargar
+            path = @"";
             InitializeComponent();
+        }
+
+        OpenFileDialog file = new OpenFileDialog();
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            if (file.ShowDialog() == DialogResult.OK) { 
+            txloadFile.Text = file.FileName;
+            path = file.FileName;
+            txBoxload2.Text = file.SafeFileName;
+            MessageBox.Show("Datos cargados correctamente.");
+        }
+
         }
     }
 }
